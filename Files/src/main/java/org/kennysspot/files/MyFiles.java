86ecd71d41fd2774d49782f0edbc40d1f274d7e6
Kenny Coltharp <kcoltharp@gmail.com>
2017-01-14 16:39:12 -0500

    Added new projects to github online

---
+++ Files/src/main/java/org/kennysspot/files/MyFiles.java
@@ -0,0 +1,20 @@
+package org.kennysspot.files;
+
+import java.nio.file.*;
+
+/**
+ *
+ * @author Kenny Coltharp
+ * @email kcoltharp@gmail.com
+ * @date August 2, 2016
+ * @version 1.0
+ */
+public class MyFiles{
+
+	Path startDirectory;
+	String directory, fileName, fileAccess;
+
+	public MyFiles(){
+	}
+
+}
